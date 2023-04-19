e0a7c8ddc4d13cd83a431a302f84987d6a5c43ca
Simone299 <simonedinello@alice.it>
2023-04-20 00:22:50 +0200

    piccola aggiustata
    tolta l'area riservata nella home page, modificato il messaggio di errore nell inserimento codice fiscale ,conferma della session in area admin,modificato il testo in dettagli

--- src/main/java/it/corso/controller/adminController.java
+++ src/main/java/it/corso/controller/adminController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("loginAdmin")
 public class adminController {
 	
 	@GetMapping
-	public String getpage7() {
+	public String getpage7(HttpSession session) {
+		
+		if(session.getAttribute("amministratore")== null)
+			return "redirect:/";
+		
 		
 		return "areaadmin";
 	}
